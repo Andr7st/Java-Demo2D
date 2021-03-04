bf592f519564f5d80d187a142a50d881316a359e
Andres Segura <segura1st@outlook.es>
2021-03-03 20:36:33 -0500

    Ejercicio de demostracion de graficas 2D co swing: Inicio

---
+++ src/andr7st/Main.java
@@ -0,0 +1,18 @@
+package andr7st;
+
+import javax.swing.*;
+
+public class Main extends JFrame {
+
+    Main() {
+        setTitle("Andr7st | Grafica2D");
+        setSize(480,480);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
